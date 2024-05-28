2dd05f16d4375d34b7db9bfd04a274381c339615
iburrese <iburres@att.netm>
2024-05-28 15:37:59 -0500

    Added code for Module 1 Lecture

--- com.utsa.lectures/src/Module1.java
+++ com.utsa.lectures/src/Module1.java
@@ -5,11 +5,21 @@
     ....
  */
 
+// This is the import section, or what is commonly referred to as the pre-processor section
+import javax.swing.JOptionPane;
+
 public class Module1 {
 
     public static void main(String[] args) {
 
-        System.out.print("Hello World");
+        String name = "";
+
+        //System.out.print("Hello World");
+
+        //JOptionPane.showMessageDialog(null,"Please enter your name");
+        name = JOptionPane.showInputDialog("Please enter your name: ");
+        JOptionPane.showMessageDialog(null, "Hello " + name + ", welcome to IS-2063");
+        System.exit(0);
     }
 
 }
